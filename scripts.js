78c64e2ad2d0e76615852216dbbbe1fd491d874d
Stag <jstag7@gmail.com>
2025-10-08 19:38:05 -1000

    added functionality to the new book button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
-const myLibrary = []
-const container = document.querySelector(".container")
+const myLibrary = [];
+const container = document.querySelector(".container");
 const newBookBtn = document.querySelector('#newBookBtn');
-const newBookDialog = document.querySelector("dialog")
+const newBookDialog = document.querySelector("dialog");
+const submitBtn = document.querySelector("button[type='submit']");
+const form = document.querySelector("#newBookForm");
+
 
 // CONSTRUCTING THE BOOK OBJECT
 function Book(title, author, pages, isRead, id) {
@@ -15,34 +18,45 @@ function Book(title, author, pages, isRead, id) {
     this.id = id;
     this.info = function() {
         if (isRead === true) {
-            return (`${this.title} by ${this.author}, ${this.pages} pages, has been read. ID: <span style="color: gray; font-family: monospace">${this.id}</span>`)
+            return (`${this.title} by ${this.author}, ${this.pages} pages, has been read. ID: <span style="color: gray; font-family: monospace">${this.id}</span>`);
         } else if (isRead === false) {
-            return (`${this.title} by ${this.author}, ${this.pages} pages, has not been read yet. ID: <span style="color: gray; font-family: monospace">${this.id}</span>`)
+            return (`${this.title} by ${this.author}, ${this.pages} pages, has not been read yet. ID: <span style="color: gray; font-family: monospace">${this.id}</span>`);
         } else {
-            return (`${this.title} by ${this.author}, ${this.pages} pages. ID: <span style="color: gray; font-family: monospace">${this.id}</span>`)
+            return (`${this.title} by ${this.author}, ${this.pages} pages. ID: <span style="color: gray; font-family: monospace">${this.id}</span>`);
         }
     };
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-    myLibrary.push(new Book(title, author, pages, isRead, crypto.randomUUID()))
+    myLibrary.push(new Book(title, author, pages, isRead, crypto.randomUUID()));
 }
 
-// EXAMPLE BOOKS
-
 function displayBooks() {
+    const bookContainer = document.querySelector("#book-container");
+    bookContainer.innerHTML = "";
     myLibrary.forEach(book => {
         const bookCard = document.createElement("div");
-        bookCard.innerHTML = book.info()
-        container.appendChild(bookCard);
+        bookCard.innerHTML = book.info();
+        bookContainer.appendChild(bookCard);
     });
 }
 
-addBookToLibrary("TheHobbit", "J. R. R. Tolken", 295, false)
-addBookToLibrary("My Book", "A DEV", 300, true)
-displayBooks()
-
-  newBookBtn.addEventListener('click', () => {
+newBookBtn.addEventListener('click', () => {
     newBookDialog.show();
-    // const
-  });
+});
+
+form.addEventListener("click", (e) => {
+    if (e.target.type == "submit") {
+        e.preventDefault();
+        newBookDialog.close();
+        let formData = new FormData(e.currentTarget)
+        addBookToLibrary(formData.get("book-name"), formData.get("author-name"), formData.get("pages"), formData.get("is-read"));
+        // formData.get(name attribute)
+        form.reset();
+        displayBooks();
+    }
+})
+
+addBookToLibrary("The Hobbit", "J. R. R. Tolken", 295, false);
+addBookToLibrary("My Book", "A DEV", 300, true);
+displayBooks();
