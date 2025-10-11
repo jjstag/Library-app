47fa56c0372a19a9f3f4d4ded2bb13df3863b31d
Stag <jstag7@gmail.com>
2025-10-11 13:47:38 -1000

    added back book.info and tweaked it so that the remove button works

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // TODO:
 // fix xss vulnerability DONE
-// make book.info return an array and change "read: true/false" to"Has been read/Not read yet"
+// make book.info return an array and change "read: true/false" to"Has been read/Not read yet" DONE
 // add a button to change read status
 // style
 
@@ -17,11 +17,20 @@ function Book(title, author, pages, isRead, id) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    this.Title = title;
-    this.Author = author;
-    this.Pages = pages;
-    this.Read = isRead;
-    this.ID = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.id = id;
+    this.info = function() {
+        let isReadText
+        if (this.isRead === false) {
+            isReadText = "Has been read"
+        } else if (this.isRead === true) {
+            isReadText = "Not read yet"
+        }
+        return [this.title, this.author, this.pages, isReadText, this.id]
+    }
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
@@ -33,19 +42,25 @@ function displayBooks() {
     bookContainer.innerHTML = "";
     myLibrary.forEach(book => {
         const bookCard = document.createElement("div");
-        bookCard.dataset.id = book.ID
+        bookCard.dataset.id = book.id
         bookContainer.appendChild(bookCard);
-        for (property in book) {
+
+        for (property of book.info()) {
             const bookPropertyP = document.createElement("p")
             bookPropertyP.style.class = "bookPropertyP"
-            bookPropertyP.textContent = property + ": " + book[property]
+            bookPropertyP.textContent = property
             bookCard.appendChild(bookPropertyP)
         }
+
         const removeBookBtn = document.createElement("button")
         removeBookBtn.style.class = "removeBookBtn"
         removeBookBtn.textContent = "Remove Book"
         bookCard.appendChild(removeBookBtn)
         removeBookBtn.addEventListener("click", (e) => {
+            const index = myLibrary.findIndex((currVal) => currVal.id === bookCard.dataset.id);
+            if (index > -1) { // only splice array when item is found
+                myLibrary.splice(index, 1); // 2nd parameter means remove one item only
+            }
             e.target.parentNode.remove()
         });
     });
@@ -70,3 +85,4 @@ form.addEventListener("click", (e) => {
 addBookToLibrary("The Hobbit", "J. R. R. Tolken", 295, false);
 addBookToLibrary("My Book", "A DEV", 300, true);
 displayBooks();
+console.log(myLibrary)
