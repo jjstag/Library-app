29b8b72f44f847adc039a7e62787d4957486ee58
Stag <jstag7@gmail.com>
2025-10-14 14:03:39 -1000

    Added a button to change read status with functionality

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 // TODO:
 // fix xss vulnerability DONE
 // make book.info return an array and change "read: true/false" to"Has been read/Not read yet" DONE
-// add a button to change read status
+// add a button to change read status DONE
+// make it work DONE
 // style
+    // change colors of book images to match
 
 const myLibrary = [];
 const container = document.querySelector(".container");
@@ -24,12 +26,20 @@ function Book(title, author, pages, isRead, id) {
     this.id = id;
     this.info = function() {
         let isReadText
-        if (this.isRead === false) {
+        if (this.isRead === true) {
             isReadText = "Has been read"
-        } else if (this.isRead === true) {
+        } else if (this.isRead === false) {
             isReadText = "Not read yet"
+        } else {
+            console.log("isreadtext does not match: " + isReadText)
         }
-        return [this.title, this.author, this.pages, isReadText, this.id]
+        return [
+            this.title,
+            this.author,
+            this.pages,
+            isReadText,
+            this.id,
+        ]
     }
 }
 
@@ -41,27 +51,53 @@ function displayBooks() {
     const bookContainer = document.querySelector("#book-container");
     bookContainer.innerHTML = "";
     myLibrary.forEach(book => {
+        //Book Card
         const bookCard = document.createElement("div");
-        bookCard.dataset.id = book.id
+        bookCard.setAttribute("class", "bookCard")
+        bookCard.dataset.id = book.id;
         bookContainer.appendChild(bookCard);
-
+        // Elements
         for (property of book.info()) {
-            const bookPropertyP = document.createElement("p")
-            bookPropertyP.style.class = "bookPropertyP"
-            bookPropertyP.textContent = property
-            bookCard.appendChild(bookPropertyP)
+            const bookPropertyP = document.createElement("p");
+            bookPropertyP.style.class = "bookPropertyP";
+            bookPropertyP.textContent = property;
+            bookCard.appendChild(bookPropertyP);
         }
+        // Toggle Read Button       
+        const toggleReadBtn = document.createElement("img");
+        if (book.isRead === true) {
+            toggleReadBtn.setAttribute("src", "./Images/book-closed.png")
+        } else if (book.isRead === false) {
+            toggleReadBtn.setAttribute("src", "./Images/book-open.png")
+        } else {
+            console.log("isReadText not defined")
+        }
+        bookCard.insertBefore(toggleReadBtn, bookCard.children[4]);
+        toggleReadBtn.addEventListener("click", (e) => {
+            const index = myLibrary.findIndex((currVal) => currVal.id === e.target.parentNode.dataset.id);
+            if (index > -1) { // only splice array when item is found
+                if (myLibrary[index].isRead == true) {
+                    myLibrary[index].isRead = false
+                } else if (myLibrary[index].isRead == false) {
+                    myLibrary[index].isRead = true
+                } else {
+                    console.log("Index.isread is neither true nor false: " + myLibrary[index].isRead)
+                }
+            }
+            displayBooks()
 
-        const removeBookBtn = document.createElement("button")
-        removeBookBtn.style.class = "removeBookBtn"
-        removeBookBtn.textContent = "Remove Book"
-        bookCard.appendChild(removeBookBtn)
+        })
+        //Remove Book Button
+        const removeBookBtn = document.createElement("button");
+        removeBookBtn.style.class = "removeBookBtn";
+        removeBookBtn.textContent = "Remove Book";
+        bookCard.appendChild(removeBookBtn);
         removeBookBtn.addEventListener("click", (e) => {
             const index = myLibrary.findIndex((currVal) => currVal.id === bookCard.dataset.id);
             if (index > -1) { // only splice array when item is found
                 myLibrary.splice(index, 1); // 2nd parameter means remove one item only
             }
-            e.target.parentNode.remove()
+            e.target.parentNode.remove();
         });
     });
 }
@@ -75,7 +111,8 @@ form.addEventListener("click", (e) => {
         e.preventDefault();
         newBookDialog.close();
         let formData = new FormData(e.currentTarget)
-        addBookToLibrary(formData.get("book-name"), formData.get("author-name"), formData.get("pages"), formData.get("is-read"));
+        addBookToLibrary(formData.get("book-name"), formData.get("author-name"), formData.get("pages"), formData.get("is-read") === "true");
+        console.log("FORMDATA ISREAD: " + formData.get("is-read"))
         // formData.get(name attribute)
         form.reset();
         displayBooks();
